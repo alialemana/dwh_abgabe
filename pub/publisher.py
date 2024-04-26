0f2baa84eed23f254f75f3c515c58dc4a2cc92de
alialemana <inf21021@lehre.dhbw-stuttgart.de>
2024-04-26 10:25:31 +0200

    before changing secret

--- pub/publisher.py
+++ pub/publisher.py
@@ -4,41 +4,42 @@ import time
 import random
 import os 
 
+# Get broker-name from environment, in this case the mosquitto-broker-service
 broker = os.getenv("BROKER")
-FIRST_RECONNECT_DELAY = 1
-RECONNECT_RATE = 2
-MAX_RECONNECT_COUNT = 12
-MAX_RECONNECT_DELAY = 60
 
+# Log-Callback Function
 def on_log(client, userdata, paho_log_level, messages):
     if paho_log_level == mqtt.LogLevel.MQTT_LOG_ERR:
         print(message)
+
+# Connect-Callback Function
 def on_connect(client, userdata, flags, reason_code):
     if reason_code.is_failure:
         print(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
     else:
-        # we should always subscribe from on_connect callback to be sure
-        # our subscribed is persisted across reconnections.
         print("connected")
-def on_disconnect(client, userdata, rc):
-    logging.info("Disconnected with result code: %s", rc)
-    reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
-    while reconnect_count < MAX_RECONNECT_COUNT:
-        logging.info("Reconnecting in %d seconds...", reconnect_delay)
-        time.sleep(reconnect_delay)
+
+# Publish-Callback Function
 def on_publish(client, userdata, mid):
     print("PUBACK erhalten für Nachricht mit id", mid)
 
+# Initialize MQTT Client with specified ID
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "publisherID-lksadhouivhlefhiuvuk7388490fshsdlfviop", clean_session=False)
+# Connect to Broker
 mqttc.connect(broker, 1883, 60)
+# Start loop
+mqttc.loop_start()
+
+# as debugging, message are counted
 msg_count=1
 while True:
-    
+    # sent payload
     jsonPayload = {
         "fin": "SNTU411STM9032444",
         "zeit": int(time.time()),
         "geschwindigkeit": random.randint(0, 50),
     }
+    # stores the answer of publishing in results
     results = mqttc.publish("DataMgmt", json.dumps(jsonPayload), qos=1)
     status = results[0]
     if status == 0:
@@ -47,7 +48,6 @@ while True:
         print("sending failed")
     msg_count +=1
     mqttc.on_publish
-    mqttc.on_disconnect
     mqttc.on_log
     print("gesendet")
     time.sleep(5)
